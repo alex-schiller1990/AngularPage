9a80e8cbe939ef03795c45ee57ebc2c49ac2c895
Alex <alex.schiller1990@web.de>
2026-02-22 13:40:11 +0100

    feat(anime): link list tiles to detail page
    Wrap each anime tile in a routerLink to /anime/:id so clicking
    navigates to the corresponding detail page. Add RouterLink import
    and hover styling on the tile links.

--- src/app/features/anime/anime-list/anime-list.ts
+++ src/app/features/anime/anime-list/anime-list.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { AnimeService } from '../anime.service';
 
 @Component({
   selector: 'app-anime-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './anime-list.html',
   styleUrl: './anime-list.css',
   standalone: true
